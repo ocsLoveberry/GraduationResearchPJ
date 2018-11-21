bc84218462b02d6bccfdd7266961850945ee155c
ocsLoveberry <ocsloveberry@gmail.com>
2018-11-21 14:05:23 +0900

    fix: 時間割から打刻検索画面にリンク貼りました

--- src/sessionManager/sessionManager.java
+++ src/sessionManager/sessionManager.java
@@ -6,14 +6,17 @@ import javax.servlet.http.HttpSession;
 public class sessionManager {
 	public void createSession(HttpServletRequest request, String seki_no) {
 
+//		セッションが存在しないか？
 		HttpSession session = request.getSession(false);
 		if(session == null) {
-
+//		セッションが既に存在するとき
 		}else {
+
 			Object loginCheck = session.getAttribute("login");
 			if(loginCheck == null) {
 				System.out.println("ログインに関するデータがありません");
 			}
+//			新しいセッションを作成する
 			session = request.getSession(true);
 			session.setAttribute("login", seki_no);
 			System.out.println("login"+session.getAttribute("login"));
